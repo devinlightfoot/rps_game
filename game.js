aaedd97a7db576f584b96c9579d37ee2c823ca9e
Devin Lightfoot <euler2171828@gmail.com>
2020-06-21 22:57:41 -0700

    Added a GUI

--- game.js
+++ game.js
@@ -1,72 +1,82 @@
 var computerSelection;
 var playerSelection;
-let huscore = 0;
-let compscore = 0;
-var count;
+var huScore = 0;
+var compScore = 0;
+refreshScore();
 function computerPlay() {
     function getRndInteger(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     }
     choice = getRndInteger(0, 3);
     if (choice == 0) {
-        computerSelection = "rock";
+        return "rock";
     }
     else if (choice == 1) {
-        computerSelection = "paper";
+        return "paper";
     }
     else {
-        computerSelection = "scissors";
+        return "scissors";
     }
 }
 function playRound(player, computer) {
-    player = playerSelection;
-    computer = computerSelection;
-    if (playerSelection === computerSelection) {
+    player.toLowerCase();
+    console.log(player);
+    console.log(computer);
+    if (player === computer) {
         count--;
-        return "You both chose the same thing, go again!";
+        alert("You both chose the same thing, go again!");
     }
-    else if ((playerSelection === "rock") && (computerSelection === "paper")) {
-        compscore++;
-        return "You lose this round, paper beats rock";
+    else if ((player === "rock") && (computer === "paper")) {
+        compScore++;
+        alert("You lose this round, paper beats rock");
     }
-    else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-        huscore++;
-        return "You won this round! Rock beats paper";
+    else if ((player === "rock") && (computer === "scissors")) {
+        huScore++;
+        alert("You won this round! Rock beats paper");
     }
-    else if ((playerSelection === "paper") && (computerSelection === "rock")) {
-        huscore++;
-        return "You won this round! Paper bears rock";
+    else if ((player === "paper") && (computer === "rock")) {
+        huScore++;
+        alert("You won this round! Paper beats rock");
     }
-    else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
-        compscore++;
-        return "You lose this round, scissors beat paper";
+    else if ((player === "paper") && (computer === "scissors")) {
+        compScore++;
+        alert("You lose this round, scissors beat paper");
     }
-    else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
-        compscore++;
-        return "You lose this round, rock beats scissors";
+    else if ((player === "scissors") && (computer === "rock")) {
+        compScore++;
+        alert("You lose this round, rock beats scissors");
     }
-    else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        huscore++;
-        return "You win this round! Scissors beat paper";
+    else if ((player === "scissors") && (computer === "paper")) {
+        huScore++;
+        alert("You win this round! Scissors beat paper");
     }
     else {
         alert("something went wrong");
     }
 }
-function game() {
-    for (count = 0; count < 5; count++) {
-        playerSelection = prompt("Please make your selection", "rock");
-        playerSelection = playerSelection.toLowerCase();
-        computerPlay();
-        playRound(playerSelection, computerSelection);
+const btns = Array.from(document.querySelectorAll("button"));
+btns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        playRound(button.dataset.key, computerPlay());
+        refreshScore();
+        console.log(huScore);
+        console.log(compScore);
+        checkWin();
+    });
+});
+function refreshScore() {
+    document.getElementById('hscore').innerHTML = huScore;
+    document.getElementById('cscore').innerHTML = compScore;
+}
+function checkWin(){
+    if(huScore === 5){
+        alert("Congratulations, you have bested the machine!");
     }
-    if (huscore > compscore) {
-        return "Congratulations, you beat the machine!";
+    else if(compScore === 5){
+        alert("The machine has bested you, better luck next time");
     }
-    else if (compscore > huscore) {
-        return "The machine has bested you, better luck next time";
+    else{
+        return;
     }
-    else {
-        alert("something went wrong");
-    }
+
 }
